ab98c3bcb44cbca17cab325fc4ef2793053622a3
Mohamed Amine Barhoumi <mohamed.amine.barhoumi.eng@gmail.com>
2024-12-31 13:56:14 +0100

    finall

--- src/components/Guestinfo.jsx
+++ src/components/Guestinfo.jsx
@@ -209,7 +209,6 @@ const Guestinfo = () => {
             type="submit"
             className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-4 hover:bg-blue-600 w-full font-semibold"
           >
-            {" "}
             Pay Now
           </button>
         </div>
